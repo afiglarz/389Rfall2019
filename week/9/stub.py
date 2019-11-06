bb8901e76ed0d90805b9558c2eeab2f0a2fe2e42
Andrew Figlarz <afiglarz@terpmail.umd.edu>
2019-11-06 15:48:38 -0500

    hw9 progress

--- week/9/stub.py
+++ week/9/stub.py
@@ -2,7 +2,7 @@
 
 import sys
 import struct
-import datetime
+
 
 
 # You can use this method to exit on failure conditions.
@@ -13,6 +13,18 @@ def bork(msg):
 # Some constants. You shouldn't need to change these.
 MAGIC = 0x8BADF00D
 VERSION = 1
+
+SECTION_ASCII = 1
+SECTION_UTF8 = 2
+SECTION_WORDS = 3
+SECTION_DWORDS = 4
+SECTION_DOUBLES = 5
+SECTION_COORD = 6
+SECTION_REFERENCE = 7
+SECTION_PNG = 8
+SECTION_GIF87 = 9
+SECTION_GIF89 = 10
+
 
 if len(sys.argv) < 2:
     sys.exit("Usage: python stub.py input_file.fpff")
@@ -26,8 +38,8 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
 magic, version = struct.unpack("<LL", data[0:8])
-timestamp, author = struct.unpack("<LQ", data[8:20])
-
+timestamp, author = struct.unpack("<L8s", data[8:20])
+(section,) = struct.unpack("<L", data[20:24])
 
 
 if magic != MAGIC:
@@ -41,11 +53,46 @@ print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
 print("TIMESTAMP: %d" % int(timestamp))
-#print("AUTHOR: %s" % hex(author).decode("hex"))
-print(hex(author))
+print("AUTHOR: %s" % str(author))
+print("SECTIONS: %d" % int(section))
 
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+
+offset = 24
+i = 0
+
+while i < int(section):
+
+    stype, slen = struct.unpack("<LL", data[offset:offset+8])
+    slen = int(slen)
+
+    if stype == SECTION_ASCII:
+        (output,) = struct.unpack(("<%ds" % slen), data[offset + 8: (offset + 8 + slen)])
+        print("ASCII OUTPUT: %s" % (output))
+        
+    elif stype == SECTION_UTF8:
+        
+        print()
+    elif stype == SECTION_WORDS:
+        print()
+    elif stype == SECTION_DWORDS:
+        print()
+    elif stype == SECTION_DOUBLES:
+        print()
+    elif stype == SECTION_COORD:
+        print()
+    elif stype == SECTION_REFERENCE:
+        print()
+    elif stype == SECTION_PNG:
+        print()
+    elif stype == SECTION_GIF87:
+        print()
+    elif stype == SECTION_GIF89:
+        print()
+        
+    offset = offset + slen + 8
+    i = i + 1
